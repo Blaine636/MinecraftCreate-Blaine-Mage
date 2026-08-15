49e292b6624a09884bf63bad9b7bbf28f69a32c4
Blaine636 <Blaine636@nowhere.com>
2026-08-14 19:42:42 -0600

    Automated server run commit: 2026-08-14 19:41:59 -06:00 to 2026-08-14 19:42:40 -06:00

--- run.bat
+++ run.bat
@@ -1,7 +1,8 @@
 @echo off
-REM Forge requires a configured set of both JVM and program arguments.
-REM Add custom JVM arguments to the user_jvm_args.txt
-REM Add custom program arguments {such as nogui} to this file in the next line before the %* or
-REM  pass them to this script directly
-java @user_jvm_args.txt @libraries/net/minecraftforge/forge/1.20.1-47.2.0/win_args.txt %*
-pause
+setlocal
+
+cd /d "%~dp0"
+
+powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0run-server.ps1"
+
+exit /b %ERRORLEVEL%
