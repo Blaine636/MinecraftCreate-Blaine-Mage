2a124bccced1485a5663f6b376d62d637d7218f9
Blaine636 <Blaine636@nowhere.com>
2026-08-16 17:13:53 -0600

    Automated server run commit: 2026-08-16 17:13:39 -06:00 to 2026-08-16 17:13:53 -06:00

--- run-server.ps1
+++ run-server.ps1
@@ -70,7 +70,7 @@ $serverProcess = Start-Process `
     -FilePath "java.exe" `
     -ArgumentList @(
         "@user_jvm_args.txt",
-        "@libraries/net/minecraftforge/forge/1.20.1-47.2.0/win_args.txt"
+        "@libraries/net/minecraftforge/forge/1.20.1-47.3.0/win_args.txt"
     ) `
     -WorkingDirectory $serverDirectory `
     -NoNewWindow `
